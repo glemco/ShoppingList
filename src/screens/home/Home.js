15d29073e3850d713a9744c32db89079b7fdefbe
gabry25 <gabry25basketball@gmail.com>
2017-12-13 19:02:10 +0100

    Added themed logo in main screen

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text, Image, Button } from 'react-native';
 import Styles from '../../styles/StyleSheet';
+import colors from '../../styles/ColorSets';
 
 import firebase from './../../service/Database';
+
+const logos = [
+  require('../../../assets/img/logo0.png'),
+  require('../../../assets/img/logo1.png'),
+  require('../../../assets/img/logo2.png'),
+  require('../../../assets/img/logo3.png'),
+  require('../../../assets/img/logo4.png'),
+  require('../../../assets/img/logo5.png'),
+];
 
 export default class Home extends Component {
     render() {
@@ -10,7 +20,7 @@ export default class Home extends Component {
             <View style={Styles().containerBackground}>
                 <Image
                     style={{width: 300, height: 300}}
-                    source={require('../../../assets/img/logo.png')}
+                    source={logos[colors.index]}
                 />
                 <Text style={[Styles().title, styles.text]}>Alfred welcomes you !</Text>
             </View>
